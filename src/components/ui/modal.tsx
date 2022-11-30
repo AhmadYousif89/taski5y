8b5e6742b969b2d4027850e1c683db0586e90e4a
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-11-30 17:00:48 +0200

    updating modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -2,7 +2,10 @@ import { SpinnerIcon } from 'assets/icons';
 
 export const Modal = () => {
   return (
-    <div className="fixed top-1/2 left-1/2 z-50 h-1/2 w-2/3 -translate-x-1/2 -translate-y-1/2 bg-color-card bg-opacity-70 ">
+    <div className="fixed top-1/2 left-1/2 z-50 flex h-1/2 w-2/3 -translate-x-1/2 -translate-y-1/2 flex-col bg-color-card bg-opacity-70">
+      <h3 className="center-absolute pb-64 text-5xl text-color-base">
+        Loading Tasks ...
+      </h3>
       <div className="center-absolute text-color-base [&>*]:h-24 [&>*]:w-24">
         <SpinnerIcon />
       </div>
