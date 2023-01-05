f81f4a271cda4b3f0af9c4973365fc95dc28c84e
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-01-05 03:42:46 +0200

    refactor sortTask

--- src/components/tasks/helpers/sort-tasks.ts
+++ src/components/tasks/helpers/sort-tasks.ts
@@ -13,30 +13,32 @@ import { Task, TaskSortOrder, TaskSortType } from 'features/types';
  */
 export const sortTasks = (
   tasks: Task[],
-  params: { sort: TaskSortOrder; type: TaskSortType }
+  { sort, type }: { sort: TaskSortOrder; type: TaskSortType }
 ): Task[] => {
   const data = tasks ? [...tasks] : [];
 
-  const sortedData = data.sort((taskA, taskB) => {
-    const { sort, type } = params;
+  const sortFn = (taskA: Task, taskB: Task) => {
     switch (type) {
-      case 'alpha': {
-        if (sort === 'asc')
-          return taskA.title.toLowerCase().localeCompare(taskB.title.toLowerCase());
-        else return taskB.title.toLowerCase().localeCompare(taskA.title.toLowerCase());
-      }
-      case 'createdAt': {
-        if (sort === 'asc') return taskA.createdAt.localeCompare(taskB.createdAt);
-        else return taskB.createdAt.localeCompare(taskA.createdAt);
-      }
-      case 'priority': {
-        if (sort === 'asc') return taskA.priority === 'Normal' ? -1 : 1;
-        else return taskB.priority === 'Normal' ? -1 : 1;
-      }
+      case 'alpha':
+        return sort === 'asc'
+          ? taskA.title.toLowerCase().localeCompare(taskB.title.toLowerCase())
+          : taskB.title.toLowerCase().localeCompare(taskA.title.toLowerCase());
+      case 'createdAt':
+        return sort === 'asc'
+          ? taskA.createdAt.localeCompare(taskB.createdAt)
+          : taskB.createdAt.localeCompare(taskA.createdAt);
+      case 'priority':
+        return sort === 'asc'
+          ? taskA.priority === 'High'
+            ? 1
+            : taskB.priority === 'High'
+            ? -1
+            : 0
+          : taskA.priority.localeCompare(taskB.priority);
       default:
-        return taskA.priority.localeCompare(taskB.priority);
+        return 0;
     }
-  });
+  };
 
-  return sortedData;
+  return data.sort(sortFn);
 };
